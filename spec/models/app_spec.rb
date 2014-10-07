6aca621081fbba594e9fbcc46831377e2c7c840e
Mario Hayashi <mario.hayashi@gmail.com>
2014-10-07 22:13:47 +0200

    Add test to app_spec.rb to ensure App name unique constraint
    Commands run:
    	bundle exec guard

--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe App do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @app = App.new(name: "TestApp")
+  end
+
+  subject { @app }
+
+  it { should respond_to(:name) }
+  it { should be_valid }
+
+  # Test uniqueness of App name.
+
+  describe "when name is not present" do
+    before { @app.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is already taken" do
+    before do
+      app_with_same_name = @app.dup
+      app_with_same_name.name = @app.name.upcase
+      app_with_same_name.save
+    end
+
+    it { should_not be_valid }
+  end
 end
